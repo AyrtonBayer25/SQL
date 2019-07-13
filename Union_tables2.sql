cf1d8d08c2518c536ccb1e4ec8c1055005707ece
AyrtonBayer <46352240+AyrtonBayer25@users.noreply.github.com>
2019-07-13 13:42:30 -0400

    Part 2 joining tables together

---
+++ Union_tables2.sql
@@ -0,0 +1,46 @@
+-- Database: join
+
+-- DROP DATABASE "join";
+
+CREATE DATABASE "join"
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_United States.1252'
+    LC_CTYPE = 'English_United States.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+	
+	DROP TABLE IF EXISTS toys;
+DROP TABLE IF EXISTS games;
+
+CREATE TABLE toys (
+    toy_id SERIAL,
+    type VARCHAR,
+    name VARCHAR
+);
+
+CREATE TABLE games (
+    game_id SERIAL,
+    type VARCHAR,
+    name VARCHAR
+);
+
+INSERT INTO toys (type, name)
+VALUES
+('sports', 'baseball'),
+('adventure', 'staff'),
+('sports', 'tennis ball'),
+('fun', 'doll');
+
+INSERT INTO games (type, name)
+VALUES
+('sports', 'tag'),
+('adventure', 'Kings Quest'),
+('sports', 'tennis'),
+('fun', 'Make believe');
+Select * from (
+Select toy_id as id, type, name from toys
+	union
+Select game_id as id, type, name from games ) ABC 
+	where id >=2;
