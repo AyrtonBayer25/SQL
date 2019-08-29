3377649c5dbab0d82aa6b8433c918ae4736d6e85
AyrtonBayer <46352240+AyrtonBayer25@users.noreply.github.com>
2019-08-29 19:56:44 -0400

    Update SQL Top 100 music.sql

--- SQL Top 100 music.sql
+++ SQL Top 100 music.sql
@@ -7,3 +7,7 @@ ORDER BY song_name DESC;
 
 ---Write a query that returns all rows from 2010 ordered by rank, with artists ordered alphabetically for each song.
 SELECT * FROM tutorial.billboard_top_100_year_end WHERE year = 2010 ORDER BY year_rank, artist;
+
+--Write a query that shows all rows for which T-Pain was a group member, 
+--ordered by rank on the charts, from lowest to highest rank (from 100 to 1).
+SELECT * FROM tutorial.billboard_top_100_year_end WHERE "group" ILIKE '%t-pain%' order by year_rank desc;
