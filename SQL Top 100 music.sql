3fbb9df83d0841c8da68dd0c6aa6a45098157108
AyrtonBayer <46352240+AyrtonBayer25@users.noreply.github.com>
2019-08-29 20:34:43 -0400

    Update SQL Top 100 music.sql

--- SQL Top 100 music.sql
+++ SQL Top 100 music.sql
@@ -11,3 +11,10 @@ SELECT * FROM tutorial.billboard_top_100_year_end WHERE year = 2010 ORDER BY yea
 --Write a query that shows all rows for which T-Pain was a group member, 
 --ordered by rank on the charts, from lowest to highest rank (from 100 to 1).
 SELECT * FROM tutorial.billboard_top_100_year_end WHERE "group" ILIKE '%t-pain%' order by year_rank desc;
+
+--Write a query that returns songs that ranked between 10 and 20 (inclusive) in 1993, 2003, or 2013. 
+--Order the results by year and rank, and leave a comment on each line of the 
+--WHERE clause to indicate what that line does
+SELECT * FROM tutorial.billboard_top_100_year_end WHERE YEAR IN (2013, 2003, 1993) --SELECT the relevant years
+and year_rank BETWEEN 10 AND 20 --limit the rank to 10-20
+ORDER BY year, year_rank;
